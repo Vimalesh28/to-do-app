ea3b8a1e1507a2102c2bb53415c4c00004fcc393
Vimalesh28 <vimaleshkvkl@gmail.com>
2022-12-08 21:15:37 +0530

    added sample java

---
+++ src/TodoApp/SampleJava.java
@@ -0,0 +1,38 @@
+package TodoApp;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class SampleJava {
+	public static void main(String []args) {
+		Scanner sc=new Scanner(System.in);
+
+		enum level{
+			HIGH,
+			MEDIUM,
+			LOW
+		}
+		String value=sc.nextLine();
+		String[] array=value.split("#");
+
+
+		System.out.println(array[0]);
+
+
+		try {
+			Date date = new SimpleDateFormat("DD.MM.YY").parse(array[1]);
+			System.out.println(date);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
+		level levelEnum =  level.valueOf(array[2]);
+		System.out.print(levelEnum);
+
+
+	}}
