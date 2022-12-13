d4cc22babd9266c28f0fabce2b25938017c08738
Vimalesh28 <vimaleshkvkl@gmail.com>
2022-12-13 12:08:31 +0530

    new file employee

--- src/TodoApp/Employee.java
+++ src/TodoApp/Employee.java
@@ -1,23 +1,24 @@
 package TodoApp;
 import java.util.*;
 public class Employee {
-	public String name;
+   
+   public String name;
 	
-	private Double salary;
+   private Double salary;
 	
-	public Employee(String nameEmp) {
+   public Employee(String nameEmp) {
 	  name=nameEmp;
-	}
-  public void setSalary(Double empsalary) {
+   }				
+   public void setSalary(Double empsalary) {
 	  salary = empsalary;
-	}
-  public void printEmployees() {
-     System.out.print(name);
-     System.out.print(salary);
-    }
-  public static void main(String[] args) {
-	Employee variable = new Employee("vimal");
-    variable.setSalary(20.0);
-    variable.printEmployees();
-}
-}
+   }
+   public void printEmployees() {
+      System.out.print(name);
+      System.out.print(salary);
+   }
+   public static void main(String[] args) {
+	  Employee variable = new Employee("vimal");
+      variable.setSalary(20.0);
+      variable.printEmployees();
+   }
+   }
