ee0c56339a13908da1f55b2407e5599537394a59
Vimalesh28 <95900470+Vimalesh28@users.noreply.github.com>
2022-12-14 11:59:12 +0530

    Add files via upload

---
+++ src/Readfile.java
@@ -0,0 +1,24 @@
+package TodoApp;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Readfile {
+
+	public static void main(String[] args) throws Exception {
+
+		File file = new File("C:\\Users\\Hp\\eclipse-workspace\\TodoApp\\src\\TodoApp\\Todoclass.java");
+		Scanner sc;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine())
+				System.out.println(sc.nextLine());
+		} catch (FileNotFoundException e) {
+			System.out.print("user does not give input");
+		}
+	}
+}
